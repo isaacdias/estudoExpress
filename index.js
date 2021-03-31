8b161df174cb09f4839ad3fda347e12fceae9d43
Isaac Dias <isaacardias@gmail.com>
2021-03-31 13:14:22 -0300

    refatorando o codigo

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
+const { response } = require('express');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Olá mundo!')
+app.get('/projects', (req, res) => {
+    const { title, owner} = req.query;
+    console.log(title);
+    console.log(owner);
+
+    res.json([
+        'projeto1',
+        'projeto2',
+    ])
+});
+
+app.post('/projects', (req, res) => {
+    return res.json([
+        'projeto1',
+        'projeto2',
+        'projeto3',
+        'projeto4',
+        'projeto5',
+    ])
 })
 
-app.listen(3000, () => 
-    console.log('servidor rodando')
-)
+app.put('/projects/:id', (req, res) => {
+    const params = req.params;
+
+    console.log(params);
+
+    return res.json([
+        'projeto50',
+        'projeto2',
+        'projeto3',
+        'projeto4',
+        'projeto5',
+    ])
+})
+
+app.delete('/projects/:id', (req, res) => {
+    return res.json([
+        'projeto50',
+        'projeto2',
+    ])
+})
+
+app.listen(3000)
