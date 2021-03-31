720bd617d40a43b10eb2e29ab7b8073c9727f72d
Isaac Dias <isaacardias@gmail.com>
2021-03-31 10:09:25 -0300

    subindo servidor e instalando o nodemon

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ const express = require('express');
 
 const app = express();
 
-console.log(app)
+app.get('/', (req, res) => {
+    res.send('Olá mundo!')
+})
+
+app.listen(3000, () => 
+    console.log('servidor rodando')
+)
